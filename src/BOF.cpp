6a474357308b74a7ea15c1df4eddb304a065f59d
leresteux <leresteuxj@gmail.com>
2021-01-22 08:30:03 +0100

    commentaire vf

--- src/BOF.cpp
+++ src/BOF.cpp
@@ -6,7 +6,10 @@ BOF::BOF(byte R_pin) {
   _pin = R_pin;
 
 }
-
+////////
+// fonctions publiques
+////////
+// regle la pin attribué au relais et les varaibles au demarrage. A mettre dans Set up 
 void BOF::begin() {
   pinMode(_pin, OUTPUT);
   _etat = false;
@@ -15,25 +18,24 @@ void BOF::begin() {
   _BPM=1;
   off();
 }
-
-void BOF::BPM(unsigned int varBPM=1) {
-	_BPM=60000/varBPM;	
-}
-
+/////
+// "allume "le relais
 void BOF::on() {
   _etat = true;
   if (_isEnable){
   digitalWrite(_pin, LOW);
   }
 }
-
+/////
+//"eteind" le relais
 void BOF::off() {
   _etat = false;
   if (_isEnable){
   digitalWrite(_pin, HIGH);
   }
 }
-
+/////
+// doit être appeler regulierement (dans void loop) pour appliquer la librairie BOF
 void BOF::process() {
 	if (_storedTime == 0) {
 		_storedTime = millis();
@@ -41,13 +43,46 @@ void BOF::process() {
 	_elapsedTime = millis()-_storedTime;
 		switchOnOff();
 	}
-  
+/////
+// active le relais (par défaut)
+void BOF::isEnable() {
+  _isEnable=true;
+}
+/////
+//desactive le relais
+void BOF::isDisable() {
+  _isEnable=false;
+  digitalWrite(_pin, HIGH);
+}
+/////
+// active ou desactive le relais en fonction de l'etat 
+void BOF::isPause() {
+  if (_isEnable){
+    isDisable();
+  }else{
+    isEnable();
+  }
+}
+/////
+// fait passer le relais d'un etat on à off en boucle ( si appeler par void process)
+// float onDuration est indiqué en milliseconde et correspond à la durée en possition on du relais
+// idem pour float offDuration
+//les var onDuration et offDuration peuvent être introduites en Beat Par Minute si la fonction BPM a ete appeler au par avant 
+// voir exemple beat_maker2
 void BOF::bling(float onDuration, float offDuration) {
 	_onDuration = _BPM*onDuration;
 	_offDuration = _BPM*offDuration;
 	_oriOffDuration = _offDuration;
 }
-
+/////
+// permet de regler void bling en BPM
+void BOF::BPM(unsigned int varBPM=1) {
+	_BPM=60000/varBPM;	
+}
+/////
+// créer un effet 
+// fadein si la valeur de varFade est comprise entre 1 et 0 (idéal etant 0.8 par exemple) 
+//fadein si la valeur de varFade est > 1 (idéal etant 1.2 par exemple)
 void BOF::fade(float varFade) {
 	if (_fadeIsRunning==false){
 		_fadeIsRunning=true;
@@ -59,6 +94,11 @@ void BOF::fade(float varFade) {
 	}
 }		
 
+////////
+//fonctions internes
+////////
+// calcule s'il doit allumer ou eteindre le relais
+// voir void process
 void BOF::switchOnOff() {
   if (_elapsedTime >= _offDuration && _etat == false) {
 		on();
@@ -75,21 +115,4 @@ void BOF::switchOnOff() {
 			_storedTime = millis();
 	}
 
-}
-
-void BOF::isEnable() {
-  _isEnable=true;
-}
-
-void BOF::isDisable() {
-  _isEnable=false;
-  digitalWrite(_pin, HIGH);
-}
-
-void BOF::isPause() {
-  if (_isEnable){
-    isDisable();
-  }else{
-    isEnable();
-  }
 }
