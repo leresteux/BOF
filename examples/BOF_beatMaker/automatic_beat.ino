3df50f2ac7f9414f078bd2b4d4e0c14d87f5ca78
julien leresteux <leresteuxj@gmail.com>
2021-03-02 22:26:23 +0100

    make music with everythinks

---
+++ examples/BOF_beatMaker/automatic_beat.ino
@@ -0,0 +1,115 @@
+/////
+// Have fun !
+// Ju'
+////
+#include <BOF.h>
+#include <Chrono.h>
+
+Chrono monchrono;
+BOF monrelais1(3);
+BOF monrelais2(4);
+BOF monrelais3(5);
+BOF monrelais4(6);
+
+unsigned int nbrBPM = 120 ;
+byte moment = 0;
+
+void setup() {
+  monrelais4.begin();
+  monrelais3.begin();
+  monrelais2.begin();
+  monrelais1.begin();
+  monrelais1.on();
+  delay(1000);
+  monrelais1.off();
+  delay(1000);
+  monrelais2.on();
+  delay(1000);
+  monrelais2.off();
+  delay(1000);
+  monrelais3.on();
+  delay(1000);
+  monrelais3.off();
+  delay(1000);
+  monrelais4.on();
+  delay(1000);
+  monrelais4.off();
+  delay(1000);
+  delay(10);
+  monrelais1.BPM(nbrBPM);
+  monrelais2.BPM(nbrBPM);
+  monrelais3.BPM(nbrBPM);
+  monrelais4.BPM(nbrBPM);
+  monrelais2.isDisable();
+  monrelais3.isDisable();
+  monrelais4.isDisable();
+  monrelais1.isDisable();
+  delay(10);
+  Serial.begin(9600);
+  monrelais1.bling(1, 1, 0);
+  monrelais1.fade(1.5);
+  monchrono.restart();
+}
+
+void loop() {
+  allProcess();
+
+
+  if (monchrono.hasPassed(10000) && moment == 0) { // returns true if it passed 1000 ms since it was started
+    monchrono.restart();
+    monrelais2.isEnabled();
+    monrelais2.bling(1, 1, 2);
+    moment++;
+  };
+  if (monchrono.hasPassed(20000) && moment == 1) { // returns true if it passed 1000 ms since it was started
+    monchrono.restart();
+    monrelais3.isEnabled();
+    monrelais3.bling(3, 1, 0);
+    moment++;
+  };
+
+  if (monchrono.hasPassed(15000) && moment == 2) { // returns true if it passed 1000 ms since it was started
+    monchrono.restart();
+    monrelais4.isEnabled();
+    monrelais4.bling(7, 1, 0);
+    moment++;
+  };
+  if (monchrono.hasPassed(20000) && moment == 3) { // returns true if it passed 1000 ms since it was started
+    monchrono.restart();
+    //monrelais1.isPause();
+    //monrelais2.isPause();
+    monrelais3.isPause();
+    moment++;
+  };
+  if (monchrono.hasPassed(10000) && moment == 4) { // returns true if it passed 1000 ms since it was started
+    monchrono.restart();
+    monrelais1.isPause();
+    moment++;
+  };
+
+  if (monchrono.hasPassed(10000) && moment == 5) { // returns true if it passed 1000 ms since it was started
+    monchrono.restart();
+    monrelais1.isPause();
+    moment++;
+  };
+
+  if (monchrono.hasPassed(10000) && moment == 6) { // returns true if it passed 1000 ms since it was started
+    monchrono.restart();
+    monrelais2.isPause();
+    moment++;
+  };
+  if (monchrono.hasPassed(30000) && moment == 7) { // returns true if it passed 1000 ms since it was started
+    monchrono.restart();
+    monrelais2.isDisable();
+    monrelais3.isDisable();
+    monrelais4.isDisable();
+    monrelais1.isDisable();
+    moment++;
+  };
+}
+void allProcess () {
+  monrelais4.process();
+  monrelais3.process();
+  monrelais2.process();
+  monrelais1.process();
+}
